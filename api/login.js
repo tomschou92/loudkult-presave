c2493cba95d02b3dadd096e355c54d5836296b6b
tomschou92 <tom@loudkult.com>
2026-03-05 15:26:11 +0100

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,9 +1,11 @@
-// api/login.js
 export default function handler(req, res) {
   const client_id = process.env.SPOTIFY_CLIENT_ID;
   const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
+
+  const { type, id } = req.query; // track or playlist + ID
+
   const scope = 'user-library-modify user-follow-modify';
-  const state = Math.random().toString(36).substring(2, 15);
+  const state = Buffer.from(JSON.stringify({ type, id })).toString('base64');
 
   const queryParams = new URLSearchParams({
     client_id,
